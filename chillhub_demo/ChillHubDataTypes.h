f6b9cb81bb1011fac9f1b5c70b9d297427aaf2c3
Rob Bultman <rob.bultman@ge.com>
2014-09-10 16:28:53 -0400

    Adding convenience header files for Chill Hub message and data types.

---
+++ chillhub_demo/ChillHubDataTypes.h
@@ -0,0 +1,14 @@
+// Chill Hub data types
+enum DataTypes {
+  arrayDataType = 0x01,
+  stringDataType = 0x02,
+  unsigned8DataType = 0x03,
+  signed8DataType = 0x04,
+  unsigned16DataType = 0x05,
+  signed16DataType = 0x06,
+  unsigned32DataType = 0x07,
+  signed32DataType = 0x08,
+  jsonDataType = 0x09,
+  booleanDataType = 0x10
+};
+
