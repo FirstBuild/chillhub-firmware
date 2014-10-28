f3282b0235aff9f8181227d122e961ff9483947a
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-10-28 16:45:23 -0400

    structured app+ firebase loadschema

--- chillhub-devices.js
+++ chillhub-devices.js
@@ -4,18 +4,21 @@ var stream = require('binary-stream');
 var sets = require('simplesets');
 var Firebase = require("firebase");
 
+var commons = require('./commons');
+var parsers = require('./parsing')
+
 var FBURI = "" // root url for Firebase
 
 var CronJob = require('cron').CronJob;
 
 function ChillhubDevice(ttyPath, receive, announce) {
 	var self = this;
-	
+	 
 	this.deviceType = '';
 	this.subscriptions = new sets.Set([]);
 	this.buf = [];
 	this.cronJobs = {};
-	self.schema = ""
+	self.schema = [];
 	
 	this.uid = ttyPath;
 	this.tty = new serial.SerialPort('/dev/'+ttyPath, { 
@@ -29,7 +32,6 @@ function ChillhubDevice(ttyPath, receive, announce) {
 	});
 	
 	//Load Schema from Firebase
-	//FIXME define deviceType
 	//!\ Asynchronous call
 	loadSchema(self.deviceType,function(data){
 		if(data){
@@ -65,7 +67,7 @@ function ChillhubDevice(ttyPath, receive, announce) {
 	
 		self.send = function(data) {
 			// parse data into the format that usb devices expect and transmit it
-			var dataBytes = parseJsonToStream(data);
+			var dataBytes = parsers.parseJsonToStream(data);
 			var writer = new stream.Writer(dataBytes.length+1, stream.BIG_ENDIAN);
 			
 			writer.writeUInt8(dataBytes.length);
@@ -78,39 +80,32 @@ function ChillhubDevice(ttyPath, receive, announce) {
 			});
 		};
 	});
-
-	function loadSchema(deviceType){
+	
+	// Load schema from Firebase for a deviceType and return values in an array
+	function loadSchema(deviceType,callback){
 		var firebase = new Firebase(FBURI);
 		var schemaRef = firebase.child('schemas').child(deviceType)
 
+		var schema = []
 		schemaRef.once('value',function(snap){
-			console.log(snap.val())
-		})
+			$.each(snap.val().split(','),function(i,v){
+				schema.push(v)
+			})
+			callback(this)
+		},schema)
+
+		//return as an array
 	}
 	
 	function cronCallback(id) {
 		return function() {
-			var msgContent =  encodeTime(id);
+			var msgContent = commons.encodeTime(id);
 			
 			self.send({
 				type: 0x05,
 				content: msgContent
 			});
 		};
-	}
-	
-	function encodeTime(id) {
-		var now = new Date();
-		var dateField = [now.getMonth(), now.getDate(), now.getHours(), now.getMinutes()];
-		if (id) 
-			dateField.splice(0, 0, id);
-		
-		return dateField.map(function(val) {
-			return {
-				numericType: 'U8',
-				numericValue: val
-			};
-		});
 	}
 	
 	function routeIncomingMessage(data) {
@@ -121,6 +116,12 @@ function ChillhubDevice(ttyPath, receive, announce) {
 			case 0x00:
 				self.deviceType = jsonData.content;
 				console.log('REGISTERed device "'+self.deviceType+'"!');
+
+				// Load Schema of this device
+				loadSchema(self.deviceType,function(data){
+					console.log("schema DATA",data)
+					self.schema = data 
+				});
 				announce();
 				break;
 			case 0x01: // subscribe to data stream
@@ -148,7 +149,7 @@ function ChillhubDevice(ttyPath, receive, announce) {
 			case 0x06: // get time
 				self.send({
 					type: 0x07,
-					content: encodeTime()
+					content: commons.encodeTime()
 				});
 				break;
 			default:
@@ -156,205 +157,7 @@ function ChillhubDevice(ttyPath, receive, announce) {
 				cloudEndpoint.set(jsonData);
 				receive(self, jsonData);
 		}	
-	}
-	
-	function parseStreamToJson(data) {
-		var getDataReadFunction = function(instream) {
-			var readFcn;
-			switch(instream.readUInt8()) {
-				case 0x00: // no data
-					readFcn = function(stream) {
-						return;
-					};
-					break;
-				case 0x01: // array
-					readFcn = parseArrayFromStream;
-					break;
-				case 0x02: // string
-					readFcn = parseStringFromStream;
-					break;
-				case 0x03: // numeric types
-					readFcn = function(stream) {
-						return stream.readUInt8();
-					};
-					break;
-				case 0x04:
-					readFcn = function(stream) {
-						return stream.readInt8();
-					};
-					break;
-				case 0x05:
-					readFcn = function(stream) {
-						return stream.readUInt16();
-					};
-					break;
-				case 0x06:
-					readFcn = function(stream) {
-						return stream.readInt16();
-					};
-					break;
-				case 0x07:
-					readFcn = function(stream) {
-						return stream.readUInt32();
-					};
-					break;
-				case 0x08:
-					readFcn = function(stream) {
-						return stream.readInt32();
-					};
-					break;
-				case 0x09: // js object
-					readFcn = parseObjectFromStream;
-					break;
-				case 0x0A: // boolean (could also be done as a uint8)
-					readFcn = parseBooleanFromStream;
-					break;
-			}
-			return readFcn;
-		};
-		
-		var parseArrayFromStream = function(instream) {
-			var length = instream.readUInt8();
-			var readFcn = getDataReadFunction(instream);
-			
-			var array = [];
-			for (var j = 0; j < length; j++) {
-				array.push(readFcn(instream));
-			}
-			return array;
-		};
-	
-		var parseStringFromStream = function(instream) {
-			var length = instream.readUInt8();
-			return instream.readAscii(length);
-		};
-		
-		var parseObjectFromStream = function(instream) {
-			var length = instream.readUInt8();
-			var obj;
-			
-			for (var i = 0; i < length; i++) {
-				var fieldName = parseStringFromStream(instream);
-				obj[fieldName] = parseDataFromStream(instream);
-			}
-			
-			return obj;
-		};
-		
-		var parseBooleanFromStream = function(instream) {
-			return (instream.readUInt8() != 0);
-		};
-			
-		var parseDataFromStream = function(instream) {
-			var readFcn = getDataReadFunction(instream);
-			return readFcn(instream);
-		};
-		
-		var reader = new stream.Reader(data, stream.BIG_ENDIAN);
-		return {
-			type: reader.readUInt8(),
-			content: parseDataFromStream(reader)
-		};
 	}	
-	
-	function parseJsonToStream(message) {
-		var parseArrayToStream = function(outstream, array, doWriteType) {
-			if (doWriteType)
-				outstream.writeUInt8(0x01);
-			outstream.writeUInt8(array.length);
-			
-			for (var j = 0 ; j < array.length; j++) {
-				parseDataToStream(outstream, array[j], (j == 0));
-			}
-		};
-		
-		var parseStringToStream = function(outstream, str, doWriteType) {
-			if (doWriteType)
-				outstream.writeUInt8(0x02);
-			outstream.writeUInt8(str.length);
-			outstream.writeAscii(str);
-		};
-		
-		var parseNumericToStream = function(outstream, num, doWriteType) {
-			// default to I16
-			if (doWriteType)
-				outstream.writeUInt8(types[i].id);
-			outstream.writeInt16(num);
-		};
-		
-		var parseNumericObjectToStream = function(obj) {
-			var NUMERIC_TYPES = {
-				U8: { fcn: 'writeUInt8', id: 0x03 },
-				U16: { fcn: 'writeUInt16', id: 0x05 },
-				U32: { fcn: 'writeUInt32', id: 0x07 },
-				I8: { fcn: 'writeInt8', id: 0x04 },
-				I16: { fcn: 'writeInt16', id: 0x06 },
-				I32: { fcn: 'writeInt32', id: 0x08 }
-			};
-			var objType = NUMERIC_TYPES[obj.numericType];
-			
-			return function(outstream, num, doWriteType) {
-				if (doWriteType) 
-					outstream.writeUInt8(objType.id);
-				outstream[objType.fcn](num.numericValue);
-			};
-		};
-		
-		var parseObjectToStream = function(outstream, obj, doWriteType) {
-			if (doWriteType)
-				outstream.writeUInt8(0x09);
-			outstream.writeUInt8(Object.keys(obj).length);
-			for (var field in obj) {
-				outstream.parseStringToStream(outstream, field, false);
-				outstream.parseDataToStream(outstream, obj[field], true);
-			}
-		};
-		
-		var parseBooleanToStream = function(outstream, bool, doWriteType) {
-			if (doWriteType)
-				outstream.writeUInt8(0x0A);
-			outstream.writeUInt8(bool?0x01:0x00);
-		};
-		
-		var parseNothingToStream = function(outstream, data, doWriteType) {
-			if (doWriteType)
-				outstream.writeUInt8(0x00);
-		};
-		
-		var parseDataToStream = function(outstream, data, doWriteType) {
-			var parseFcn;
-			switch ( Object.prototype.toString.call(data) ) {
-				case '[object String]':
-					parseFcn = parseStringToStream;
-					break;
-				case '[object Boolean]':
-					parseFcn = parseBooleanToStream;
-					break;
-				case '[object Number]':
-					parseFcn = parseNumericToStream;
-					break;
-				case '[object Array]':
-					parseFcn = parseArrayToStream;
-					break;
-				case '[object Null]':
-				case '[object Undefined]':
-					parseFcn = parseNothingToStream;
-					break;
-				default:
-					if (data.numericType)
-						parseFcn = parseNumericObjectToStream(data);
-					else
-						parseFcn = parseObjectToStream;
-					break;
-			}
-			parseFcn(outstream, data, doWriteType);
-		};
-		
-		var writer = new stream.Writer(255, stream.BIG_ENDIAN);
-		writer.writeUInt8(message.type);
-		parseDataToStream(writer, message.content, true);
-		return writer.toArray();
-	}
 
 	self.cleanup = function() {
 		for (var j in self.cronJobs)
@@ -367,11 +170,13 @@ var devices = {};
 exports.init = function(receiverCallback, deviceListCallback) {
 	var filePattern = /^ttyACM[0-9]{1,2}$/;
 	
+	//similar to announce()
 	var listDevices = function() {
 		var devList = [];
 		for (var dev in devices)
 			devList.push(devices[dev].deviceType);
 		deviceListCallback(devList);
+		//TODO add logic to send list of devices to Firebase
 	}
 	
 	var callbackWrapper = function(dev, msg) {
